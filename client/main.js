69661ab2a505e0ca94a4b7f5589b6484cd188ea1
Ravi Kumar <ravithesun02@gmail.com>
2020-10-01 22:03:47 +0530

    Server done

---
+++ client/main.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+import Helloworld from './Helloworld';
+
+render(
+    <Helloworld/>,document.getElementById('root')
+)
---
+++ config/config.js
@@ -0,0 +1,8 @@
+const config={
+    env:process.env.NODE_ENV || 'development',
+    port:process.env.PORT || 8000,
+    jwtSecret:process.env.JWT_SECRET || 'abcdef-1234-@#$%',
+    mongoUri:process.env.MONGODB_URI || process.env.MONGO_HOST|| 'mongodb://'+(process.env.IP || 'localhost')+ ':'+(process.env.MONGO_PORT ||'27017')+'/socialapp'
+}
+
+export default config;
